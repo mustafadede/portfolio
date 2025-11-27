527e7c5b36c02c27208aff24b03bc0e7e9f985d0
Mustafa DEDE <95627279+mustafadede@users.noreply.github.com>
2025-11-27 16:55:58 +0300

    feat: seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Mustafa Dede",
-  description: "Mustafa Dede's personal portfolio website",
+  description: "Mustafa Dede'nin kişisel portföy sitesi",
+  openGraph: {
+    title: "Mustafa Dede",
+    description: "Mustafa Dede'nin kişisel portföy sitesi",
+    url: "https://mustafadede.com",
+    siteName: "Mustafa Dede Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "tr_TR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mustafa Dede",
+    description: "Mustafa Dede'nin kişisel portföy sitesi",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -26,6 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="canonical" href="https://mustafadede.com" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black!`}
       >
@@ -34,6 +56,21 @@ export default function RootLayout({
           {children}
         </ClientWrapper>
       </body>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: "Mustafa Dede",
+            url: "https://mustafadede.com",
+            sameAs: [
+              "https://github.com/mustafadede",
+              "https://linkedin.com/in/mustafadede",
+            ],
+          }),
+        }}
+      />
     </html>
   );
 }
