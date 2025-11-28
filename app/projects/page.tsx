ee6e30d072d82d41783e7f6eb625a89e9c948166
Mustafa DEDE <95627279+mustafadede@users.noreply.github.com>
2025-11-28 19:42:37 +0300

    fix: projects hero section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,30 +20,42 @@ function projects() {
         frequency={1}
         warpStrength={1}
         mouseInfluence={0.8}
-        parallax={0.6}
+        parallax={0.8}
         noise={0.08}
         className="absolute! top-0 z-0"
       />
-      <motion.div
-        initial={{ opacity: 0, y: 20, filter: "blur(20px)" }}
-        animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-        exit={{ opacity: 0, y: 20, filter: "blur(20px)" }}
-        transition={{ delay: 0.4, duration: 0.9 }}
-        className="relative z-10 flex flex-col gap-4"
-      >
-        <SharePalLogo />
+      <div className="flex flex-col items-center justify-center gap-4">
         <motion.p
-          initial={{ opacity: 0, filter: "blur(10px)" }}
-          whileInView={{
-            opacity: 1,
-            filter: "blur(0px)",
-          }}
-          transition={{ duration: 1.2, delay: 1.9 }}
-          className="text-white italic font-extralight text-xl"
+          initial={{ opacity: 0, y: 20, filter: "blur(20px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          exit={{ opacity: 0, y: 20, filter: "blur(20px)" }}
+          transition={{ delay: 0.2, duration: 0.9 }}
+          className="text-white z-10 font-extralight text-md flex items-center justify-center gap-2 border rounded-3xl border-accent/40 bg-white/20 backdrop-blur-4xl px-4 py-1"
         >
-          Film ve dizi aşıklarına ait bir platform
+          <span className="w-1 h-1 rounded-full bg-fuchsia-600"></span>
+          Mustafa'nın Favorisi
         </motion.p>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 20, filter: "blur(20px)" }}
+          animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+          exit={{ opacity: 0, y: 20, filter: "blur(20px)" }}
+          transition={{ delay: 0.4, duration: 0.9 }}
+          className="relative z-10 flex flex-col gap-4 items-center select-none"
+        >
+          <SharePalLogo />
+          <motion.p
+            initial={{ opacity: 0, filter: "blur(10px)" }}
+            whileInView={{
+              opacity: 1,
+              filter: "blur(0px)",
+            }}
+            transition={{ duration: 1.2, delay: 1.9 }}
+            className="text-white italic font-extralight text-center text-md md:text-xl"
+          >
+            Film ve dizi tutkunlarına ait bir sosyal medya uygulaması
+          </motion.p>
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
